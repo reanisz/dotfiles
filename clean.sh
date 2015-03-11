966b58a7814118aa025afa923495cea2bfd3ad6b
reanisz <reannml@gmail.com>
2015-03-11 11:27:13 +0900

    [setup] fix

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+rm $HOME/.vimrc
+rm $HOME/.vimrc.plugin
+rm $HOME/.vim
+rm $HOME/.zshrc
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -s $PWD/vim/.vimrc $HOME/.vimrc
 ln -s $PWD/vim/.vimrc.plugin $HOME/.vimrc.plugin
+ln -s $PWD/vim/.vim $HOME/.vim
 touch $HOME/.vimrc.local
 ln -s $PWD/zsh/.zsrhc $HOME/.zshrc
 touch $HOME/.zshrc.local
