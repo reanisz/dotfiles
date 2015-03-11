8c92f1f1317e58df86dca10bc080f4f44c290175
reanisz <reannml@gmail.com>
2015-03-11 15:18:59 +1100

    [vim]

---
+++ installNeoBundle.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ ln -s $PWD/vim/.vimrc $HOME/.vimrc
 ln -s $PWD/vim/.vimrc.plugin $HOME/.vimrc.plugin
 ln -s $PWD/vim/.vim $HOME/.vim
 touch $HOME/.vimrc.local
-ln -s $PWD/zsh/.zsrhc $HOME/.zshrc
+ln -s $PWD/zsh/.zshrc $HOME/.zshrc
 touch $HOME/.zshrc.local
 ln -s $PWD/tmux/.tmux.conf $HOME/.tmux.conf 
