a5e944ec64e5b88111f9ff014271bacaf6ff7982
Kiri Nakata <kiri.nakata@mixi.co.jp>
2016-08-19 07:16:59 +0000

    mirrorlistのバックアップをとるように変更

--- setupArchlinux.sh
+++ setupArchlinux.sh
@@ -1,3 +1,4 @@
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 sudo cp arch/mirrorlist /etc/pacman.d/mirrorlist
 sudo pacman -S --noconfirm archlinux-keyring
 sudo pacman -Syu --noconfirm
