15b136f8135caaf2ba9bbf2dd7b5c90cddbf7361
Reanisz <reannml@gmail.com>
2016-10-11 07:38:34 +0000

    Archlinuxセットアップを修正

--- setupArchlinux.sh
+++ setupArchlinux.sh
@@ -2,7 +2,8 @@ sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 sudo cp arch/mirrorlist /etc/pacman.d/mirrorlist
 sudo pacman -S --noconfirm archlinux-keyring
 sudo pacman -Syu --noconfirm
+sudo pacman-db-upgrade
 
 sudo pacman -S --noconfirm zsh
 
-sudo chsh -s /bin/zsh
+chsh -s /bin/zsh
