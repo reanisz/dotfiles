3888f8d023b3bd282e0a27fb65e6de04c72b617a
Reanisz <reannml@gmail.com>
2017-04-06 08:28:09 +0000

    setupArchlinuxの場所を移動

--- arch/setupArchlinux.sh
+++ arch/setupArchlinux.sh
@@ -1,5 +1,5 @@
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-sudo cp arch/mirrorlist /etc/pacman.d/mirrorlist
+sudo cp ./mirrorlist /etc/pacman.d/mirrorlist
 sudo pacman -S --noconfirm archlinux-keyring
 sudo pacman -Syu --noconfirm
 sudo pacman-db-upgrade
