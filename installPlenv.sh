4990a948b88019c0fbc98112daeea5ab94f02da8
Reanisz <reannml@gmail.com>
2017-04-06 08:27:11 +0000

    plenvのインストールはここに必要ない気がするので削除

--- installPlenv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git clone git://github.com/tokuhirom/plenv.git ~/.plenv
-echo 'export PATH=$HOME/.plenv/bin:$PATH' >> ~/.zshrc.local
-echo 'eval "$(plenv init -)"' >> ~/.zshrc.local
-git clone git://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/
